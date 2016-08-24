52de2e50b8a93ab5ae07ae7c8a707c87e40a029d
Andrew Stacey <loopspace@mathforge.org>
2016-08-24 23:52:51 +0100

    Refactored to makes tiles their own class.

--- script.js
+++ script.js
@@ -23,6 +23,16 @@ var ops = [
     }
 ];
 
+var numbers = [];
+var tilesets = [
+    [
+	[25,50,75,100],
+	[1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+    ]
+];
+var tileset = 0;
+var tiles = new Tiles();
+
 function getTarget(a) {
     var tops,
 	n,
@@ -70,35 +80,134 @@ function getRandomInt(min, max) {
 }
 
 function init() {
-    var ipt = document.getElementById('numbers');
-    ipt.addEventListener('change',setNumbers);
-    var frm = document.getElementById('form');
-    frm.addEventListener('submit',function(e) {e.preventDefault();});
+    initTiles();
+}
+
+function setSize() {
+    tiles.setScale(7,4);
+}
+
+function initTiles() {
+    tiles.setScale(7,4);
+    var vals = shuffle(tilesets[tileset][0]);
+    var tile;
+    for (var i = 0; i < 4; i++) {
+	tile = tiles.newTile();
+	tile.setPosition(i + 1.5,0,"north east");
+	tile.setSize(1,1);
+	tile.show();
+	tile.hideContents();
+	tile.setContents(vals[i]);
+	tile.onClick(pickTile);
+    }
+    vals = shuffle(tilesets[tileset][1]);
+    for (var j = 1; j < 4; j++) {
+	for (var i = 0; i < 7; i++) {
+	    if (i + j > 1) {
+		tile = tiles.newTile();
+		tile.setPosition(i,j,"north east");
+		tile.setSize(1,1);
+		tile.show();
+		tile.hideContents();
+		tile.setContents(vals[j * 7 + i - 8]);
+		tile.onClick(pickTile);
+	    }
+	}
+    }
+}
+
+function pickTile(tile,e) {
+    tile.showContents();
+    tile.offClick();
+    numbers.push(tile);
+    if (numbers.length == 6) {
+	moveTiles();
+    }
+}
+
+function moveTiles() {
+    tiles.foreachTile(function(t) {
+	t.offClick();
+	t.offScreen("south",true);
+    });
+    numbers.sort(
+	function(a,b) {
+	    if (a.contents < b.contents) {
+		return -1;
+	    } else if (a.contents == b.contents) {
+		return 0;
+	    } else {
+		return 1;
+	    }
+	}
+    );
+    for (var k = 0; k < numbers.length; k++) {
+	numbers[k].killAnimation();
+    	numbers[k].moveTo(k+.5,0,"north west",2);
+    }
+    tiles.doAfterAnimation(setTarget);
+}
+
+function setTarget() {
+    var nums = [];
+    tiles.foreachTile(function(t) {
+	nums.push(t.contents);
+    });
+    var tgt = getTarget(nums);
+    var tgttile = tiles.newTile();
+    tgttile.setSize(2,1);
+    tgttile.setContents("Target:<br>" + tgt.target);
+    tgttile.setLines(2);
+    tgttile.showContents();
+    tgttile.show();
+    var p = tiles.ScreenAnchor("south");
+    tgttile.setPosition(p.x,p.y,"north");
+    tgttile.moveTo(p.x,1,"north",1);
+    var first = tiles.newTile();
+    var op = tiles.newTile();
+    var second = tiles.newTile();
+    var equals = tiles.newTile();
+    var answer = tiles.newTile();
+    first.setSize(0,0);
+    first.show();
+    first.showContents();
+    first.setPosition(1.25,2.5,"centre")
+    first.resizeTo(1,1,"centre",1);
+    op.setSize(0,0);
+    op.show();
+    op.showContents();
+    op.setPosition(2,2.5,"centre")
+    op.resizeTo(.5,.5,"centre",1);
+    second.setSize(0,0);
+    second.show();
+    second.showContents();
+    second.setPosition(2.75,2.5,"centre")
+    second.resizeTo(1,1,"centre",1);
+    equals.setSize(0,0);
+    equals.show();
+    equals.showContents();
+    equals.setPosition(3.5,2.5,"centre")
+    equals.resizeTo(.5,.5,"centre",1);
+    equals.setContents("=");
+    answer.setSize(0,0);
+    answer.show();
+    answer.showContents();
+    answer.setPosition(4.25,2.5,"centre")
+    answer.resizeTo(1,1,"centre",1);
+}
+
+function shuffle(a) {
+    var b = [];
+    var k,c;
+    for (var i = 0; i < a.length; i++) {
+	k = getRandomInt(i,a.length);
+	c = b[i] || a[i];
+	b[i] = b[k] || a[k];
+	b[k] = c;
+    }
+    return b;
 }
 
 window.addEventListener('load',init,false);
-
-function setNumbers(e) {
-    e.preventDefault();
-    var lst = e.target.value;
-    var re = /\s*[;,]\s*/;
-    var arr = lst.split(re);
-    var num = [];
-    for (var i = 0; i < arr.length; i++) {
-	num.push(parseInt(arr[i]));
-    }
-    var tgt = getTarget(num);
-    var tgtelt = document.getElementById('target');
-    tgtelt.innerHTML = tgt.target;
-    var lstelt = document.getElementById('route');
-    lstelt.innerHTML = '';
-    var li,txt;
-    for (var i = 0; i < tgt.route.length; i++) {
-	li = document.createElement('li');
-	txt = document.createTextNode(tgt.route[i]);
-	li.appendChild(txt);
-	lstelt.appendChild(li);
-    }
-    return false;
-}
+window.addEventListener('resize',setSize,false);
 
---
+++ tile.js
@@ -0,0 +1,344 @@
+var edge,
+    interpolate,
+    getPositionFromAnchor,
+    getAnchorFromPosition
+;
+
+Tiles = function() {
+    this.tiles = [];
+    var self = this;
+    window.requestAnimationFrame(function(dt) {self.doAnimation(dt) });
+}
+
+Tiles.prototype.doAfterAnimation = function(f) {
+    this.afterAnimation = f;
+}
+
+Tiles.prototype.addTile = function(t) {
+    t.parent = this;
+    t.setAll();
+    this.tiles.push(t);
+}
+
+Tiles.prototype.newTile = function() {
+    var t = new Tile();
+    this.addTile(t);
+    return t;
+}
+
+Tiles.prototype.removeTile = function(t) {
+    var index;
+    for (var k = 0; k < this.tiles.length; k++) {
+	if (this.tiles[k] == t) {
+	    index = k;
+	    break;
+	}
+    }
+    this.tiles.splice(index,1);
+}
+
+Tiles.prototype.foreachTile = function(f) {
+    if (typeof f === 'function') {
+	for (var k = 0; k < this.tiles.length; k++) {
+	    f(this.tiles[k]);
+	}
+    } else if (typeof f === 'string') {
+	for (var k = 0; k < this.tiles.length; k++) {
+	    this.tiles[k][f]();
+	}
+    }
+}
+
+Tiles.prototype.doAnimation = function(dt) {
+    var finished = true;
+    var self = this;
+    this.foreachTile(function (t) {if (t.update(dt)) {finished = false} } );
+    if (finished && this.afterAnimation) {
+	this.afterAnimation();
+	this.afterAnimation = null;
+    }
+    window.requestAnimationFrame(function(dt) { self.doAnimation(dt) });
+}
+
+Tiles.prototype.setScale = function(x,y) {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var s = Math.floor(Math.min(w/x,h/y));
+    var fs = Math.floor((s - 40)/3);
+    this.scale = s;
+    this.fontsize = fs;
+    this.foreachTile('rescale');
+}
+
+Tiles.prototype.ScreenAnchor = function(a) {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var p = getAnchorFromPosition(w,h,0,0,a);
+    p.x /= this.scale;
+    p.y /= this.scale;
+    return p;
+}
+
+function edge (t) {
+    return Math.min(1,Math.max(0, t));
+}
+
+function interpolate (t,a,b) {
+    t = edge(t);
+    return (1 - t) * a + t * b;
+}
+
+function getPositionFromAnchor(w,h,x,y,a) {
+    if (a == "north") {
+	x -= w/2;
+    } else if (a == "north east") {
+	x -= w;
+    } else if (a == "east") {
+	x -= w;
+	y -= h/2;
+    } else if (a == "south east") {
+	x -= w;
+	y -= h;
+    } else if (a == "south") {
+	x -= w/2;
+	y -= h;
+    } else if (a == "south west") {
+	y -= h;
+    } else if (a == "west") {
+	y -= h/2;
+    } else if (a == "centre") {
+	x -= w/2;
+	y -= h/2;
+    }
+    return {x: x, y: y};
+}
+
+function getAnchorFromPosition(w,h,x,y,a) {
+    if (a == "north") {
+	x += w/2;
+    } else if (a == "north east") {
+	x += w;
+    } else if (a == "east") {
+	x += w;
+	y += h/2;
+    } else if (a == "south east") {
+	x += w;
+	y += h;
+    } else if (a == "south") {
+	x += w/2;
+	y += h;
+    } else if (a == "south west") {
+	y += h;
+    } else if (a == "west") {
+	y += h/2;
+    } else if (a == "centre") {
+	x += w/2;
+	y += h/2;
+    }
+    return {x: x, y: y};
+}
+
+Tile = function() {
+    this.element = document.createElement('div');
+    this.element.classList.add('tile');
+    document.body.appendChild(this.element);
+    this.width = 0;
+    this.height = 0;
+    this.left = 0;
+    this.top = 0;
+    this.fgColour = '#000';
+    this.bgColour = '#ccc';
+    this.border = 10;
+    this.margin = 10;
+    this.lines = 1;
+}
+
+Tile.prototype.show = function() {
+    this.element.style.display = 'inline-block';
+}
+
+Tile.prototype.hide = function() {
+    this.element.style.display = 'none';
+}
+
+Tile.prototype.setElement = function(k) {
+    var s = this.parent.scale;
+    var fs = this.parent.fontsize;
+    if (k == "height") {
+	var h, pt;
+	h = this.height/2 * s + this.lines*fs/2 - this.border - this.margin;
+	pt = this.height/2 * s - this.lines*fs/2 - this.border - this.margin;
+	this.element.style.height = h + 'px';
+	this.element.style.paddingTop = pt + 'px';
+    } else if (k == "width") {
+	this.element.style.width = (this.width * s - 2*this.border - 2*this.margin) + 'px';
+    } else if (k == "foreground") {
+	this.element.style.color = this.fgColour;
+    } else if (k == "background") {
+	this.element.style.backgroundColor = this.bgColour;
+	this.element.style.borderColor = this.bgColour;
+    } else if (k == "border") {
+	this.element.style.borderWidth = this.border + 'px';
+	this.element.style.borderRadius = this.border + 'px';
+    } else if (k == "top") {
+	this.element.style.top = ( this.top * s + this.margin) + 'px';
+    } else if (k == "left") {
+	this.element.style.left = ( this.left * s + this.margin) + 'px';
+    } else if (k == "fontSize") {
+	this.element.style.fontSize = fs + 'px';
+    } else {
+	this.element.style[k] = ( this[k] * s ) + 'px';
+    }
+}
+
+Tile.prototype.setBorder = function(b) {
+    this.border = b;
+    this.setElement("border");
+}
+
+Tile.prototype.setMargin = function(b) {
+    this.margin = b;
+}
+
+Tile.prototype.setPosition = function(x,y,a) {
+    var p = getPositionFromAnchor(this.width,this.height,x,y,a);
+    this.left = p.x;
+    this.top = p.y;
+    this.setElement("left");
+    this.setElement("top");
+}
+
+Tile.prototype.setSize = function(w,h) {
+    this.width = w;
+    this.height = h;
+    this.setElement("width");
+    this.setElement("height");
+}
+
+Tile.prototype.setColours = function(fg,bg) {
+    this.fgColour = fg || this.fgColour;
+    this.bgColour = bg || this.bgColour;
+    this.setElement("foreground");
+    this.setElement("background");
+}
+
+Tile.prototype.rescale = function() {
+    this.setElement("left");
+    this.setElement("top");
+    this.setElement("width");
+    this.setElement("height");
+    this.setElement("fontSize");
+}
+
+Tile.prototype.setAll = function() {
+    this.setElement("left");
+    this.setElement("top");
+    this.setElement("width");
+    this.setElement("height");
+    this.setElement("fontSize");
+    this.setElement("foreground");
+    this.setElement("background");
+}
+
+Tile.prototype.setContents = function(s) {
+    this.element.innerHTML = s;
+    this.contents = s;
+}
+
+Tile.prototype.setLines = function(n) {
+    this.lines = n;
+}
+
+Tile.prototype.showContents = function() {
+    this.element.style.color = this.fgColour;
+}
+
+Tile.prototype.hideContents = function() {
+    this.element.style.color = this.bgColour;
+}
+
+Tile.prototype.onClick = function(f) {
+    var self = this;
+    var fn = function(e) { f(self,e) };
+    this.clickFn = fn;
+    this.element.addEventListener('click',fn,false);
+}
+
+Tile.prototype.offClick = function() {
+    if (this.clickFn) {
+	this.element.removeEventListener('click',this.clickFn,false);
+	this.clickFn = null;
+    }
+}    
+
+Tile.prototype.moveTo = function(x,y,a,dt) {
+    this.isAnimating = true;
+    this.duration = dt * 1000;
+    this.startTime = performance.now();
+    var p = getPositionFromAnchor(this.width,this.height,x,y,a);
+    this.targetX = p.x;
+    this.targetY = p.y;
+    this.originX = this.left;
+    this.originY = this.top;
+    this.targetW = this.width;
+    this.targetH = this.height;
+    this.originW = this.width;
+    this.originH = this.height;
+}
+
+Tile.prototype.resizeTo = function(w,h,a,dt) {
+    this.isAnimating = true;
+    this.duration = dt * 1000;
+    this.startTime = performance.now();
+    var p = getAnchorFromPosition(this.width,this.height,this.left,this.top,a);
+    var q = getPositionFromAnchor(w,h,p.x,p.y,a);
+    this.targetX = q.x;
+    this.targetY = q.y;
+    this.originX = p.x;
+    this.originY = p.y;
+    this.targetW = w;
+    this.targetH = h;
+    this.originW = this.width;
+    this.originH = this.height;
+}
+
+Tile.prototype.killAnimation = function() {
+    this.isAnimating = false;
+    this.finishAnimation = null;
+}
+
+Tile.prototype.offScreen = function(a,b) {
+    if (b) {
+	this.finishAnimation = this.destroy;
+    } else {
+	this.finishAnimation = this.hide;
+    }
+    var p = this.parent.ScreenAnchor(a);
+    this.moveTo(this.x,p.y,"north west",2);
+}
+
+Tile.prototype.update = function(t) {
+    if (!this.isAnimating) {
+	return false;
+    }
+    t -= this.startTime;
+    t /= this.duration;
+    this.left = interpolate(t,this.originX,this.targetX);
+    this.top = interpolate(t,this.originY,this.targetY);
+    this.width = interpolate(t,this.originW,this.targetW);
+    this.height = interpolate(t,this.originH,this.targetH);
+    this.rescale();
+    if (t > 1) {
+	if (this.finishAnimation) {
+	    this.finishAnimation();
+	    this.finishAnimation = null;
+	}
+	this.isAnimating = false;
+    }
+    return true;
+}
+
+Tile.prototype.destroy = function() {
+    document.body.removeChild(this.element);
+    this.parent.removeTile(this);
+}
